5e13a2cd2a33865327a0a0990560663b34776c3e
mynameisrattaket <rattaket.w@kkumail.com>
2025-03-02 17:23:00 +0700

    -

---
+++ trading-journal-frontend/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    parser: '@babel/eslint-parser', // ใช้ Babel แทน parser ที่เป็นฟังก์ชัน
+    extends: ['next', 'eslint:recommended'],
+    rules: {
+      // คุณสามารถตั้งค่ากฎเพิ่มเติมได้ที่นี่
+    },
+  };
+  
